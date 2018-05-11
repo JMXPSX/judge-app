38ea4d7a221544381c8af84d6bd222bed5b109b8
daryl.j.o.rubinos <daryl.j.o.rubinos@UPPT-7464HLT-LX.dir.svc.accenture.com>
2018-05-12 00:32:10 +0800

    adding login redirect

---
+++ src/main/java/com/judge/dredd/controller/ViewController.java
@@ -0,0 +1,16 @@
+package com.judge.dredd.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class ViewController {
+
+	
+	@GetMapping("/login")
+	public RedirectView redirect() {
+		System.out.println("Test");
+		return new RedirectView("/");
+	}
+}
