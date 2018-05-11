cfc1bdc4db495643ef392b93a5e4c7d56a984a43
daryl.j.o.rubinos <daryl.j.o.rubinos@UPPT-7464HLT-LX.dir.svc.accenture.com>
2018-05-12 01:51:46 +0800

    commenting redirect

--- src/main/java/com/judge/dredd/controller/ViewController.java
+++ src/main/java/com/judge/dredd/controller/ViewController.java
@@ -4,13 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
-@Controller
+//@Controller
 public class ViewController {
 
 	
-	@GetMapping("/login")
-	public RedirectView redirect() {
-		System.out.println("Test");
-		return new RedirectView("/");
-	}
+//	@GetMapping("/login")
+//	public RedirectView redirect() {
+//		System.out.println("Test");
+//		return new RedirectView("/");
+//	}
 }
