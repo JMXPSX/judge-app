8035a2d9c7f802b0c0d52afb065ccde4c3b9080e
Leuterio <eldon.i.a.leuterio@accenture.com>
2018-05-24 21:22:09 +0800

    updates

---
+++ src/main/java/com/judge/dredd/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,19 @@
+package com.judge.dredd.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.judge.dredd.dto.CategoryDTO;
+import com.judge.dredd.service.CategoryService;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+	@Override
+	public List<CategoryDTO> getCategoriesByEvent(long eventId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
