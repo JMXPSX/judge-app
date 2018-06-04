23987e0cc2d35ea5c084de9aee87cdef7da61211
Leuterio <eldon.i.a.leuterio@accenture.com>
2018-06-04 22:09:19 +0800

    categ grp by

--- src/main/java/com/judge/dredd/repository/CategoryRepository.java
+++ src/main/java/com/judge/dredd/repository/CategoryRepository.java
@@ -16,6 +16,6 @@ public interface CategoryRepository extends CrudRepository<Category, Long>{
 	
 	public List<Category> findByEntries_judges_userId(long appUserId);
 	
-	@Query(value = "select c.* from category c, tabulator t, entry e where c.event_id = t.event_id and t.entry_id = e.entry_id and c.category_id = e.category_id and t.user_id = ?1", nativeQuery=true)
+	@Query(value = "select c.* from category c, tabulator t, entry e where c.event_id = t.event_id and t.entry_id = e.entry_id and c.category_id = e.category_id and t.user_id = ?1 group by category_id", nativeQuery=true)
 	public List<Category> findCategoriesByUserId(long appUserId);
 }
