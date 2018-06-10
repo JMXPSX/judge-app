a1bed594717db9241cf96e8a7bc31081530e81e5
daryl.j.o.rubinos <daryl.j.o.rubinos@UPPT-7464HLT-LX.dir.svc.accenture.com>
2018-06-11 00:03:29 +0800

    Websocket update

--- src/main/java/com/judge/dredd/conf/WebSocketConfig.java
+++ src/main/java/com/judge/dredd/conf/WebSocketConfig.java
@@ -2,23 +2,26 @@ package com.judge.dredd.conf;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/");
-    }
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+        // the endpoint for websocket connections
+        registry.addEndpoint("/stomp").setAllowedOrigins("*").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/");
+
+        // use the /app prefix for others
+        config.setApplicationDestinationPrefixes("/app");
     }
 
 }
--- src/main/java/com/judge/dredd/controller/PushController.java
+++ src/main/java/com/judge/dredd/controller/PushController.java
@@ -2,8 +2,10 @@ package com.judge.dredd.controller;
 
 import java.sql.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -14,16 +16,19 @@ import com.judge.dredd.dto.CommentsDTO;
 @Controller
 @EnableScheduling
 public class PushController {
-
+	
+	@Autowired
+	private SimpMessagingTemplate webSocket;
+	
 	private int array[] = new int[] { 1, 2, 3, 4 };
 	private String words[] = new String[] { "Nice Entry", "Booo!", "Magnificent", "Unrealistic" };
 
 	int index = 0;
 
 	@MessageMapping("/comment")
-	@SendTo("/topic/comments")
-	@SubscribeMapping("/topic/comments")
-	@Scheduled(fixedRate=5000)
+//	@SendTo("/topic/comments")
+//	@SubscribeMapping("/topic/comments")
+	@Scheduled(fixedRate=10000)
 	public CommentsDTO greeting() throws Exception {
 		CommentsDTO comments = new CommentsDTO();
 		comments.setComment(words[index]);
@@ -35,6 +40,8 @@ public class PushController {
 		} else {
 			index++;
 		}
+		
+		webSocket.convertAndSend("/notification", comments);
 		return comments;
 	}
 
