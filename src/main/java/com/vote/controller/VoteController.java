30e37f24e42054b3a9e1bf5e41c592ae24f33185
Leuterio <eldon.i.a.leuterio@accenture.com>
2018-07-30 18:05:26 +0800

    stomp

--- src/main/java/com/vote/controller/VoteController.java
+++ src/main/java/com/vote/controller/VoteController.java
@@ -19,7 +19,9 @@ public class VoteController {
 	@PostMapping("/vote/event/{eventId}/id/{participantId}/entry/{boothId}")
 	public ResponseEntity<?> vote (@RequestParam long eventId, @RequestParam long participantId, @RequestParam long boothId){
 		String msg = voteService.vote(eventId, participantId, boothId);
-		voteService.getResults(eventId);
+		if("done".equalsIgnoreCase(msg)){
+			voteService.getResults(eventId);
+		}
 		return new ResponseEntity<>(msg, HttpStatus.INTERNAL_SERVER_ERROR);
 				
 	}
