ecb988fcb3d7af61eb3d858649f4f149f31eac0f
Leuterio <eldon.i.a.leuterio@accenture.com>
2018-07-30 16:47:16 +0800

    cors

--- src/main/java/com/judge/dredd/conf/CorsFilter.java
+++ src/main/java/com/judge/dredd/conf/CorsFilter.java
@@ -1,5 +1,7 @@
 package com.judge.dredd.conf;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -10,6 +12,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -17,15 +20,31 @@ import org.springframework.stereotype.Component;
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
+	
+	@Value("${app.allowedOrigins}")
+	private String allowedOrigins;
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         final HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Expose-Headers", "Authorization, Server-Authorization");
+        final HttpServletRequest request = (HttpServletRequest) req;
+        
+        List<String> origins = Arrays.asList(allowedOrigins.split(","));
+        
+        String origin;
+		if ((origin = request.getHeader("Origin")) != null && origins.contains(origin)) {
+			response.setHeader("Access-Control-Allow-Origin" , origin);
+			response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+			response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Origin, Content-Type, Accept");
+			response.setHeader("Access-Control-Expose-Headers", "Authorization, Server-Authorization");
+			response.setHeader("Access-Control-Max-Age", "3600");
+		}
+        
+//        response.setHeader("Access-Control-Allow-Origin", "*");
+//        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+//        response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+//        response.setHeader("Access-Control-Max-Age", "3600");
+//        response.setHeader("Access-Control-Expose-Headers", "Authorization, Server-Authorization");
         
         if ("OPTIONS".equalsIgnoreCase(((HttpServletRequest) req).getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
